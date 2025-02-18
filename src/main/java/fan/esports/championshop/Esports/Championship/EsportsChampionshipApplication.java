b7492e073e099968277a5b5d595c5d5ddeda4fb6
Raphael <praphael5@gmail.com>
2025-02-18 15:59:11 -0300

    First commit

---
+++ src/main/java/fan/esports/championshop/Esports/Championship/EsportsChampionshipApplication.java
@@ -0,0 +1,13 @@
+package fan.esports.championshop.Esports.Championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportsChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsChampionshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fan/esports/championshop/Esports/Championship/EsportsChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package fan.esports.championshop.Esports.Championship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
