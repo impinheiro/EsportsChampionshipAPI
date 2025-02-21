3591ba7a2c6f55dd23a1958c4b14b4aadd0031c2
Raphael <praphael5@gmail.com>
2025-02-21 16:50:43 -0300

    feat: Start the communication between layers for teams

--- src/main/java/fan/esports/championship/Esports/Championship/core/gateway/TeamGateway.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/gateway/TeamGateway.java
@@ -1,4 +1,13 @@
 package fan.esports.championship.Esports.Championship.core.gateway;
 
+import fan.esports.championship.Esports.Championship.core.domain.Team;
+
+import java.util.List;
+
 public interface TeamGateway {
+    Team findTeamById(String id);
+    public List<Team> findAllTeams();
+    Team createTeam(Team team);
+    Team updateTeam(Team team, String id);
+    void deleteTeam(String id);
 }
--- src/main/java/fan/esports/championship/Esports/Championship/infrastructure/beans/BeanConfigurationTeam.java
+++ src/main/java/fan/esports/championship/Esports/Championship/infrastructure/beans/BeanConfigurationTeam.java
@@ -1,4 +1,16 @@
 package fan.esports.championship.Esports.Championship.infrastructure.beans;
 
+import fan.esports.championship.Esports.Championship.core.gateway.TeamGateway;
+import fan.esports.championship.Esports.Championship.core.usecases.team.GetAllTeamsCase;
+import fan.esports.championship.Esports.Championship.core.usecases.team.GetAllTeamsImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanConfigurationTeam {
+
+    @Bean
+    public GetAllTeamsCase getAllTeamsCase(TeamGateway teamGateway) {
+        return new GetAllTeamsImpl(teamGateway);
+    }
 }
