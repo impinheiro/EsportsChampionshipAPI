d1a8e2a65f3a1b3cb0c284d5b95e764fd407ce0f
Raphael <praphael5@gmail.com>
2025-02-21 16:18:39 -0300

    fix(core): Add list of registrations as attribute

--- src/main/java/fan/esports/championship/Esports/Championship/core/domain/Championship.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/domain/Championship.java
@@ -4,10 +4,11 @@ import fan.esports.championship.Esports.Championship.core.enums.ChampionshipForm
 import fan.esports.championship.Esports.Championship.core.enums.ChampionshipType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record Championship(String id, String name, String gameName, ChampionshipType type,
                            Integer capacity, double subscriptionValue, ChampionshipFormat format,
                            String rules,LocalDateTime startDate, LocalDateTime endDate,
                            String location, String  coverImage, String logoImage,
-                           String awardDescription) {
+                           String awardDescription, List<Registrations> registrations) {
 }
