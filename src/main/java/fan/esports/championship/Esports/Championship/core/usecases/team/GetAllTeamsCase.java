a64fd599318e2cbaeda5d9df96c02ff2291dbc32
Raphael <praphael5@gmail.com>
2025-02-21 16:49:17 -0300

    feat(core): Start the implementation of @Get all teams functionality

--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/GetAllTeamsCase.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/GetAllTeamsCase.java
@@ -1,4 +1,9 @@
 package fan.esports.championship.Esports.Championship.core.usecases.team;
 
+import fan.esports.championship.Esports.Championship.core.domain.Team;
+
+import java.util.List;
+
 public interface GetAllTeamsCase {
+    public List<Team> findAllTeams();
 }
--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/GetAllTeamsImpl.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/GetAllTeamsImpl.java
@@ -1,4 +1,20 @@
 package fan.esports.championship.Esports.Championship.core.usecases.team;
 
-public class GetAllTeamsImpl {
+import fan.esports.championship.Esports.Championship.core.domain.Team;
+import fan.esports.championship.Esports.Championship.core.gateway.TeamGateway;
+
+import java.util.List;
+
+public class GetAllTeamsImpl implements GetAllTeamsCase {
+
+    private final TeamGateway teamGateway;
+
+    public GetAllTeamsImpl(TeamGateway teamGateway) {
+        this.teamGateway = teamGateway;
+    }
+
+    @Override
+    public List<Team> findAllTeams() {
+        return teamGateway.findAllTeams();
+    }
 }
