9ac511e09ee24504ff125d531d04499aa0889e1e
Raphael <praphael5@gmail.com>
2025-02-24 22:48:20 -0300

    feat: Cover the case where a team is instantiated with a null members list to create a empty array list

--- src/main/java/fan/esports/championship/Esports/Championship/core/domain/Team.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/domain/Team.java
@@ -2,7 +2,13 @@ package fan.esports.championship.Esports.Championship.core.domain;
 
 import fan.esports.championship.Esports.Championship.core.gateway.UserGateway;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record Team(String id, String name, List<User> members){
+    public Team {
+        if (members == null) {
+            members = new ArrayList<>();
+        }
+    }
 }
--- src/main/java/fan/esports/championship/Esports/Championship/infrastructure/dtos/TeamDTO.java
+++ src/main/java/fan/esports/championship/Esports/Championship/infrastructure/dtos/TeamDTO.java
@@ -1,6 +1,12 @@
 package fan.esports.championship.Esports.Championship.infrastructure.dtos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record TeamDTO(String id, String name, List<UserDTO> members) {
+    public TeamDTO{
+        if(members == null){
+            members = new ArrayList<>();
+        }
+    }
 }
