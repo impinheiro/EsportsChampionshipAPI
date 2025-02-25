0f753e1cd6f2ec0909eeea514dfa056aa7b3d487
Raphael <praphael5@gmail.com>
2025-02-24 22:49:17 -0300

    feat: Add structure to a UpdateTeam request

--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/UpdateTeamCase.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/UpdateTeamCase.java
@@ -1,4 +1,7 @@
 package fan.esports.championship.Esports.Championship.core.usecases.team;
 
+import fan.esports.championship.Esports.Championship.core.domain.Team;
+
 public interface UpdateTeamCase {
+    Team execute(Team team, String id);
 }
--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/UpdateTeamCaseImpl.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/UpdateTeamCaseImpl.java
@@ -1,4 +1,18 @@
 package fan.esports.championship.Esports.Championship.core.usecases.team;
 
-public class UpdateTeamCaseImpl {
+import fan.esports.championship.Esports.Championship.core.domain.Team;
+import fan.esports.championship.Esports.Championship.core.gateway.TeamGateway;
+
+public class UpdateTeamCaseImpl implements UpdateTeamCase {
+
+    private final TeamGateway teamGateway;
+    public UpdateTeamCaseImpl(TeamGateway teamGateway) {
+        this.teamGateway = teamGateway;
+    }
+
+    @Override
+    public Team execute(Team team, String id) {
+        teamGateway.updateTeam(team, id);
+        return team;
+    }
 }
