e24515cf4d896600964860538420b11138c59869
Raphael <praphael5@gmail.com>
2025-03-06 10:23:29 -0300

    feat: Implement a exception catch on the search for user

--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/user/FindUserByIdCaseImpl.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/user/FindUserByIdCaseImpl.java
@@ -2,6 +2,7 @@ package fan.esports.championship.Esports.Championship.core.usecases.user;
 
 import fan.esports.championship.Esports.Championship.core.domain.User;
 import fan.esports.championship.Esports.Championship.core.gateway.UserGateway;
+import fan.esports.championship.Esports.Championship.infrastructure.exceptions.UserNotFoundException;
 
 public class FindUserByIdCaseImpl implements FindUserByIdCase {
 
@@ -12,6 +13,9 @@ public class FindUserByIdCaseImpl implements FindUserByIdCase {
     }
     @Override
     public User execute(String id) {
+        if(!userGateway.exists(id)){
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
         return userGateway.findById(id);
     }
 }
