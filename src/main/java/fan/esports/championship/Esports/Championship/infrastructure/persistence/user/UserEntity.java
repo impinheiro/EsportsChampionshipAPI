d448f3bf293072a4a22d8059abcb0e4150a720a6
Raphael <praphael5@gmail.com>
2025-03-23 12:54:48 -0300

    feat: Implement UserDetails on UserEntity as required for user authentication

--- src/main/java/fan/esports/championship/Esports/Championship/infrastructure/persistence/user/UserEntity.java
+++ src/main/java/fan/esports/championship/Esports/Championship/infrastructure/persistence/user/UserEntity.java
@@ -4,11 +4,15 @@ import fan.esports.championship.Esports.Championship.core.enums.UserRole;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Document(collection = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     private String id;
@@ -112,5 +116,35 @@ public class UserEntity {
                 ", profilePhoto='" + profilePhoto + '\'' +
                 ", role=" + role +
                 '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
