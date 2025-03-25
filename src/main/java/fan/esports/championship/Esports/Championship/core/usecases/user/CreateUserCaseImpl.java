17ffd675f593d26c3c4c711eca8261fdccd95c4d
Raphael <praphael5@gmail.com>
2025-03-25 02:05:05 -0300

    feat: Add a exception response to when a user already exists with a given email or nickname

--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/user/CreateUserCaseImpl.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/user/CreateUserCaseImpl.java
@@ -2,6 +2,7 @@ package fan.esports.championship.Esports.Championship.core.usecases.user;
 
 import fan.esports.championship.Esports.Championship.core.domain.User;
 import fan.esports.championship.Esports.Championship.core.gateway.UserGateway;
+import fan.esports.championship.Esports.Championship.infrastructure.exceptions.UserAlreadyExistsException;
 
 public class CreateUserCaseImpl implements CreateUserCase {
 
@@ -13,6 +14,10 @@ public class CreateUserCaseImpl implements CreateUserCase {
 
     @Override
     public User execute(User user) {
+        if(userGateway.existsByEmail(user.email()))
+            throw new UserAlreadyExistsException("This email is already registered");
+        else if(userGateway.existsByNickname(user.nickname()))
+            throw new UserAlreadyExistsException("This nickname is already on use");
         return userGateway.create(user);
     }
 }
