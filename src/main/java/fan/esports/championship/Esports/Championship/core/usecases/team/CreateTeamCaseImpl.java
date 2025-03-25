e734db4d941ad73b1990e6552003bae7f54bf15e
Raphael <praphael5@gmail.com>
2025-03-25 03:55:52 -0300

    feat: Add condition to only add a registered player

--- src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/CreateTeamCaseImpl.java
+++ src/main/java/fan/esports/championship/Esports/Championship/core/usecases/team/CreateTeamCaseImpl.java
@@ -1,19 +1,28 @@
 package fan.esports.championship.Esports.Championship.core.usecases.team;
 
 import fan.esports.championship.Esports.Championship.core.domain.Team;
+import fan.esports.championship.Esports.Championship.core.domain.User;
 import fan.esports.championship.Esports.Championship.core.gateway.TeamGateway;
+import fan.esports.championship.Esports.Championship.core.gateway.UserGateway;
+import fan.esports.championship.Esports.Championship.infrastructure.exceptions.UserNotFoundException;
 
 public class CreateTeamCaseImpl implements CreateTeamCase {
 
     private final TeamGateway teamGateway;
+    private final UserGateway userGateway;
 
-    public CreateTeamCaseImpl(TeamGateway teamGateway) {
+    public CreateTeamCaseImpl(TeamGateway teamGateway, UserGateway userGateway) {
         this.teamGateway = teamGateway;
+        this.userGateway = userGateway;
     }
 
     @Override
     public Team execute(Team team) {
-        Team newTeam = teamGateway.createTeam(team);
-        return newTeam;
+        for(User user : team.members()){
+            if(!userGateway.exists(user.id()) || !userGateway.existsByNickname(user.nickname()) || !userGateway.existsByEmail(user.email())){
+                throw new UserNotFoundException("User " + user.nickname() + " not found on database. Please register first");
+            }
+        }
+         return teamGateway.createTeam(team);
     }
 }
