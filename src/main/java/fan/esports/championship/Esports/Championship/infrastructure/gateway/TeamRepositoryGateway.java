823e83a24d371f9c4c190cf1a33305f980541cce
Raphael <praphael5@gmail.com>
2025-03-25 23:10:19 -0300

    style: Remove non used imports

--- src/main/java/fan/esports/championship/Esports/Championship/infrastructure/gateway/TeamRepositoryGateway.java
+++ src/main/java/fan/esports/championship/Esports/Championship/infrastructure/gateway/TeamRepositoryGateway.java
@@ -2,13 +2,10 @@ package fan.esports.championship.Esports.Championship.infrastructure.gateway;
 
 import fan.esports.championship.Esports.Championship.core.domain.Team;
 import fan.esports.championship.Esports.Championship.core.gateway.TeamGateway;
-import fan.esports.championship.Esports.Championship.infrastructure.dtos.UserDTO;
 import fan.esports.championship.Esports.Championship.infrastructure.mappers.TeamEntityMapper;
 import fan.esports.championship.Esports.Championship.infrastructure.mappers.UserEntityMapper;
 import fan.esports.championship.Esports.Championship.infrastructure.persistence.team.TeamEntity;
 import fan.esports.championship.Esports.Championship.infrastructure.persistence.team.TeamRepository;
-import fan.esports.championship.Esports.Championship.infrastructure.persistence.user.UserEntity;
-import fan.esports.championship.Esports.Championship.infrastructure.persistence.user.UserRepository;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
