3fc60e72c76703d5117894b5e144173adaecae00
Raphael <praphael5@gmail.com>
2025-03-31 19:26:58 -0300

    feat: Create a mapper entity-domain for user registrations

--- src/main/java/fan/esports/championship/Esports/Championship/infrastructure/mappers/registration/RegistrationEntityMapper.java
+++ src/main/java/fan/esports/championship/Esports/Championship/infrastructure/mappers/registration/RegistrationEntityMapper.java
@@ -1,4 +1,23 @@
 package fan.esports.championship.Esports.Championship.infrastructure.mappers.registration;
 
+import fan.esports.championship.Esports.Championship.core.domain.Registration;
+import fan.esports.championship.Esports.Championship.infrastructure.mappers.user.UserEntityMapper;
+import fan.esports.championship.Esports.Championship.infrastructure.persistence.registration.RegistrationEntity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RegistrationEntityMapper {
+
+    private final UserEntityMapper userEntityMapper;
+
+    public RegistrationEntityMapper(UserEntityMapper userEntityMapper) {
+        this.userEntityMapper = userEntityMapper;
+    }
+
+    public RegistrationEntity toEntity(Registration registration) {
+        return new RegistrationEntity(userEntityMapper.toEntity(registration.user()),registration.id());
+    }
+    public Registration toDomain(RegistrationEntity registrationEntity) {
+        return new Registration(registrationEntity.getId(), userEntityMapper.toDomain(registrationEntity.getUser()));
+    }
 }
